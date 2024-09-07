5240f86d56ad506546ee8d4081c464831c04d182
Keith <keith.appleyard04@gmail.com>
2024-09-07 22:12:38 +0100

    Changed project to load from different link with dynamically loaded tables

--- webScraper.py
+++ webScraper.py
@@ -1,22 +1,24 @@
-import requests
+from selenium import webdriver
 from bs4 import BeautifulSoup
+import time
 
-#Analyse every team
-baseUrl = 'https://fbref.com'
-data = requests.get(f'{baseUrl}/en/comps/9/Premier-League-Stats')
-soup = BeautifulSoup(data.text, 'html.parser')
-table = soup.select('table.stats_table')[0]
-teams = table.findAll('td', attrs={'data-stat':'team'})
+#Dictionaries for each position
+positions = {
+    'Goalkeepers': [],
+    'Defenders': [],
+    'Midfielders': [],
+    'Forwards': []
+}
 
-#Get information about specific team
-team = 'Manchester Utd'
-teamUrl = ''
-for i in teams:
-    if i.text.strip() == team:
-        teamUrl = i.find('a')['href']
+#Load the website to access dynamically loaded table
+driver = webdriver.Chrome()
+url = 'https://fbref.com/en/comps/9/stats/Premier-League-Stats'
+driver.get(url)
+time.sleep(5)
 
-#Load data from specic team
-data = requests.get(baseUrl + teamUrl)
-soup = BeautifulSoup(data.text, 'html.parser')
-table = soup.select('table.stats_table')[0].find('tbody')
-players = table.findAll('th', attrs={'data-stat':'player'})
+data = driver.page_source
+soup = BeautifulSoup(data, 'html.parser')
+table = soup.find('table', id='stats_standard')
+driver.quit()
+
+playerNames = table.findAll('td', attrs={'data-stat':'player'})
