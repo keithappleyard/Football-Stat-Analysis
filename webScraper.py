7ee986458487d1cdbf391e7ce7adba1207ff061f
Keith <keith.appleyard04@gmail.com>
2024-09-07 23:20:19 +0100

    Extracted data for every player and sorted them into array depending on position

--- webScraper.py
+++ webScraper.py
@@ -1,4 +1,6 @@
+import csv
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 from bs4 import BeautifulSoup
 import time
 
@@ -9,16 +11,33 @@ positions = {
     'Midfielders': [],
     'Forwards': []
 }
-
 #Load the website to access dynamically loaded table
-driver = webdriver.Chrome()
+options = Options()
+options.add_argument("--headless")
+driver = webdriver.Chrome(options=options)
 url = 'https://fbref.com/en/comps/9/stats/Premier-League-Stats'
 driver.get(url)
 time.sleep(5)
 
+#Scrape the data
 data = driver.page_source
 soup = BeautifulSoup(data, 'html.parser')
 table = soup.find('table', id='stats_standard')
 driver.quit()
 
-playerNames = table.findAll('td', attrs={'data-stat':'player'})
+#Extract headers from table
+headers = [header.text for header in table.find('thead').findAll('th')]
+
+#Extract player data
+for player in table.findAll('tr', class_=lambda x: x != 'thead'):
+    data = [data.text for data in player.findAll('td')]
+    if(data):
+        if('GK' in data[2]):
+            positions['Goalkeepers'].append(data)
+        elif('DF' in data[2]):
+            positions['Defenders'].append(data)
+        elif('MF' in data[2]):
+            positions['Midfielders'].append(data)
+        elif('FW' in data[2]):
+            positions['Forwards'].append(data)
+
